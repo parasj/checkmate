bc48000a8d826b737ea745a3be32c20046434e26
Paras Jain <parasj96@gmail.com>
2019-09-18 18:33:22 -0700

    Clean capitalization

--- remat/core/solvers/common.py
+++ remat/core/solvers/common.py
@@ -8,17 +8,17 @@ from remat.core.dfgraph import DFGraph
 SOLVER_DTYPE = np.int
 
 
-def setup_implied_s_backwards(g: DFGraph, S: np.ndarray = None):
+def setup_implied_s_backwards(g: DFGraph, s: np.ndarray = None):
     """
     Given a backward graph, this function will set the appropriate items in S to 1 in order
     to satisfy no-recompute rules during backwards optimization.
     """
-    S = S if S is not None else np.zeros((g.size, g.size), dtype=remat.core.solvers.common.SOLVER_DTYPE)
-    Vbwd = set(g.v) - set(g.vfwd)
-    for (start, end) in g.induce_subgraph(Vbwd):
+    s = s if s is not None else np.zeros((g.size, g.size), dtype=remat.core.solvers.common.SOLVER_DTYPE)
+    vbwd = list(set(g.v) - set(g.vfwd))
+    for (start, end) in g.induce_subgraph(vbwd):
         for t in range(start + 1, end + 1):
-            S[t, start] = 1
-    return S
+            s[t, start] = 1
+    return s
 
 
 def gen_s_matrix_fixed_checkpoints(g: DFGraph, segment_set: Set[int]):
@@ -68,7 +68,7 @@ def gen_s_matrix_fixed_checkpoints(g: DFGraph, segment_set: Set[int]):
     return S
 
 
-def solve_r_opt(G: DFGraph, S: np.ndarray):
+def solve_r_opt(g: DFGraph, s: np.ndarray):
     """Find the optimal recomputation pattern given caching decisions.
     Given S, E = [(i, j)] where node j depends on the result of node i,
     find R that minimizes cost, satisfies constraints. Assumes recomputation
@@ -77,22 +77,22 @@ def solve_r_opt(G: DFGraph, S: np.ndarray):
     NOTE: Does NOT check if memory limits are exceeded.
     Enforcing R[t,i] != S[t,i] does not seem to be necessary.
     """
-    T = S.shape[0]
-    assert S.shape[1] == T
+    T = s.shape[0]
+    assert s.shape[1] == T
 
-    R = np.eye(T, dtype=S.dtype)  # Enforce R_t,t = 1
+    R = np.eye(T, dtype=s.dtype)  # Enforce R_t,t = 1
     # Enforce S_{t+1,v} <= S_{t,v} + R_{t,v},
     # i.e. R_{t,v} >= S_{t+1,v} - S_{t,v}
-    S_diff = S[1:] - S[:-1]
-    R[:-1] = R[:-1] | (R[:-1] < S_diff)
+    sdiff = s[1:] - s[:-1]
+    R[:-1] = R[:-1] | (R[:-1] < sdiff)
     # Create reverse adjacency list (child -> parents, i.e. node -> dependencies)
     adj = [[] for v in range(T)]
-    for (u, v) in G.edge_list:
+    for (u, v) in g.edge_list:
         adj[v].append(u)
     # Enforce R_{t,v} <= R_{t,u} + S_{t,u} for all (u, v) \in E
     for t in range(T):
         for v in range(t, -1, -1):
             for u in adj[v]:
-                if R[t, v] > R[t, u] + S[t, u]:
+                if R[t, v] > R[t, u] + s[t, u]:
                     R[t, u] = 1
     return R
