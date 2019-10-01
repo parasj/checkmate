c470dde2d254eed6afb5c631a3a219da15a9dd2c
Paras Jain <parasj96@gmail.com>
2019-10-01 13:49:07 -0700

    Move plotting code

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
         # "psutil",
         # "setproctitle",
         "pytest",
-        "keras_segmentation @ https://github.com/ajayjain/image-segmentation-keras/archive/master.zip#egg=keras_segmentation-0.2.0remat"
+        "keras_segmentation @ https://github.com/ajayjain/image-segmentation-keras/archive/master.zip#egg=keras_segmentation-0.2.0remat",
     ]
 )
