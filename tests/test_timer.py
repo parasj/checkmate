5d4674a39036b729effe02152826673ad118334e
Paras Jain <parasj96@gmail.com>
2019-10-17 15:29:42 -0700

    Multi OS testing (#31)
    * Multi-os testing
    
    * Rename
    
    * Fix variable name
    
    * Remove broken windows build
    
    * Timer test is flaky on CI mac instance
    
    * rm macos, add py3.8
    
    * Update pythonpackage.yml

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -9,6 +9,4 @@ def test_timer():
     for t in np.arange(0.05, 0.2, 0.02):
         with Timer("test") as timer:
             time.sleep(t)
-        measured_time_err = abs(float(timer.elapsed) - t) / t
-        assert measured_time_err <= 0.25  # 25% error is generous, but else this test is flaky
         assert float(timer.elapsed) >= t
