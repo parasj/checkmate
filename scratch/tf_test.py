303e05d70016a5985c6282ed690a2e1357e80326
Ani Nrusimha <aninrusimha@berkeley.edu>
2019-11-07 14:59:14 -0800

    add copy and basic scheduling

--- scratch/tf_test.py
+++ scratch/tf_test.py
@@ -1,10 +1,12 @@
 import logging
 
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras import layers
+from copy import deepcopy
 
 #from tensorflow.python.ops import gradients_util as tfg
-from remat.remat.core.schedule import OperaterEvaluation, AllocateRegister, DeallocateRegister, Schedule
+from remat.core.schedule import OperatorEvaluation, AllocateRegister, DeallocateRegister, Schedule
 
 #tf.compat.v1.disable_eager_execution()
 logging.basicConfig(level=logging.DEBUG)
@@ -56,15 +58,62 @@ grad_conc = get_grads.get_concrete_function(mlp,
             mlp.trainable_variables)
     
 op_list = grad_conc.graph.get_operations()
+exclude_list = ["Placeholder", "ReadVariableOp", "Const", "BroadcastGradientArgs", "Fill"]
+
+def copy_op(op, new_name): #taken from "tensorflow/contrib/copy_graph/python/util/copy_elements.py"
+    nnd = deepcopy(op.node_def)
+    nnd.name = new_name
+    nod = deepcopy(op.op_def)
+    output_types = op._output_types[:]
+    input_types = op._input_types[:]
+    control_inputs = op.control_inputs[:]
+    new_op = tf.Operation(nnd, op.graph,  list(op.inputs), output_types, control_inputs, input_types, op, nod)
+    return new_op
+
+
+op_list = [op for op in op_list if op.type not in exclude_list]
+
 op_dict = dict(zip(op_list, range(len(op_list))))
 
 adj_list = {}
+#what should we not mess with
+for i in range(len(op_list)):
+    adj_list[i] = []
 for op in op_list:
     in_id = op_dict[op]
-    adj_list[in_id] = []
     for out in op.outputs:
-        adj_list[in_id].append(op_dict[out.op])
+        for cons in out.consumers():
+            if cons in op_list:
+                #adj_list[in_id].append(op_dict[cons])
+                adj_list[op_dict[cons]].append(in_id)
 
-#schedule
+#all forward schedule
+schedule = []
+reg_counter = 0
+needed = {k: len(v) for k,v in adj_list.items()}
+print(needed)
 
+for op in op_list:
+    iid = op_dict[op]
+    schedule.append(AllocateRegister(iid, iid, np.prod(op.outputs[0].shape)))
+
+    schedule.append(OperatorEvaluation(iid, adj_list[iid], iid, 9,  False, True))
+
+    for inp in op.inputs:
+        if inp.op in op_list:
+            needed[op_dict[inp.op]] -= 1
+            if needed[op_dict[inp.op]] == 0:
+                schedule.append(DeallocateRegister(iid, iid))
+print(schedule)
+
+registers = [None] * len(op_list)
+#run the schedule
+for i, inst in enumerate(schedule):
+    if type(inst) == OperatorEvaluation:
+
+
+        args = [registers[i] for i in inst.arg_regs]
+        
+        
+        
 
