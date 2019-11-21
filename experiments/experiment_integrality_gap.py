ae73625461fc2bcdd2d6f30b49750e7552d99f75
Paras Jain <parasj96@gmail.com>
2019-11-21 14:43:37 -0800

    Print statements using logging

--- experiments/experiment_integrality_gap.py
+++ experiments/experiment_integrality_gap.py
@@ -1,11 +1,12 @@
 import argparse
+import logging
 
 from experiments.common.definitions import remat_data_dir
 from experiments.common.graph_plotting import plot
 from remat.core.dfgraph import gen_linear_graph
 from remat.core.enum_strategy import ImposedSchedule
 from remat.core.solvers.lower_bound_lp import lower_bound_lp_relaxation
-from remat.core.solvers.strategy_approx_lp import solve_approx_lp_deterministic
+from remat.core.solvers.strategy_approx_lp import solve_approx_lp_deterministic_sweep
 from remat.core.solvers.strategy_griewank import solve_griewank
 from remat.core.solvers.strategy_optimal_ilp import solve_ilp_gurobi
 
@@ -20,6 +21,8 @@ def parse_args():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
     # Set parameters
     args = parse_args()
     N = args.num_layers
@@ -38,11 +41,11 @@ if __name__ == "__main__":
 
         griewank = solve_griewank(g, B)
 
-        print("--- Solving LP relaxation for lower bound")
+        logging.info("--- Solving LP relaxation for lower bound")
         lb_lp = lower_bound_lp_relaxation(g, B, approx=APPROX, eps_noise=EPS_NOISE, imposed_schedule=IMPOSED_SCHEDULE)
         plot(lb_lp, False, save_file=scratch_dir / "CHECKMATE_LB_LP.png")
 
-        print("--- Solving ILP")
+        logging.info("--- Solving ILP")
         ilp = solve_ilp_gurobi(g, B, approx=APPROX, eps_noise=EPS_NOISE, imposed_schedule=IMPOSED_SCHEDULE,
                                solve_r=SOLVE_R)
         ilp_feasible = ilp.schedule_aux_data.activation_ram <= B
@@ -53,16 +56,16 @@ if __name__ == "__main__":
 
         approx_ratio_actual, approx_ratio_ub = float("inf"), float("inf")
         try:
-            print("--- Solving deterministic rounting of LP")
-            approx_lp_determinstic = solve_approx_lp_deterministic(g, B, approx=APPROX, eps_noise=EPS_NOISE,
-                                                                   imposed_schedule=IMPOSED_SCHEDULE)
+            logging.info("--- Solving deterministic rounting of LP")
+            approx_lp_determinstic = solve_approx_lp_deterministic_sweep(g, B, approx=APPROX, eps_noise=EPS_NOISE,
+                                                                         imposed_schedule=IMPOSED_SCHEDULE)
             if approx_lp_determinstic.schedule_aux_data:
                 approx_ratio_ub = approx_lp_determinstic.schedule_aux_data.cpu / lb_lp.schedule_aux_data.cpu
                 approx_ratio_actual = approx_lp_determinstic.schedule_aux_data.cpu / ilp.schedule_aux_data.cpu
         except Exception as e:
-            print("WARN: exception in solve_approx_lp_deterministic:", e)
+            logging.error("WARN: exception in solve_approx_lp_deterministic")
+            logging.exception(e)
 
-        print(
-            ">>> N={} B={} ilp_feasible={} lb_lp_feasible={} integrality_gap={:.3f} approx_ratio={:.3f}-{:.3f} time_ilp={:.3f} time_lp={:.3f} speedup={:.3f}".format(
-                N, B, ilp.feasible, lb_lp.feasible, integrality_gap, approx_ratio_actual, approx_ratio_ub,
-                ilp.solve_time_s, lb_lp.solve_time_s, speedup))
+        logging.info(f">>> N={N} B={B} ilp_feasible={ilp.feasible} lb_lp_feasible={lb_lp.feasible}"
+                     f" integrality_gap={integrality_gap:.3f} approx_ratio={approx_ratio_actual:.3f}-{approx_ratio_ub:.3f}"
+                     f" time_ilp={ilp.solve_time_s:.3f} time_lp={lb_lp.solve_time_s:.3f} speedup={speedup:.3f}")
