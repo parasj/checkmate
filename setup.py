893245aa8707ce15615c02557cbc0a66e3ef1224
Paras Jain <parasj96@gmail.com>
2019-12-02 14:24:31 -0800

    Add test-time dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     packages=['remat'],  # find_packages()
     python_requires='>=3.6',
     install_requires=[
-        "graphviz",
         "matplotlib",  # this is only used once in the core remat package
         "numpy",
         "pandas",
@@ -19,12 +18,17 @@ setup(
         'gpu': ['tensorflow-gpu>=2.0.0'],
         'eval': [
             "keras_segmentation @ https://github.com/ajayjain/image-segmentation-keras/archive/master.zip#egg=keras_segmentation-0.2.0remat",
+            "graphviz",
             "python-dotenv",
             "ray>=0.7.5",
             "redis",
             "scipy",
             "seaborn",
             "tqdm",
+        ],
+        'test': [
+            "keras_segmentation @ https://github.com/ajayjain/image-segmentation-keras/archive/master.zip#egg=keras_segmentation-0.2.0remat",
+            "graphviz",
         ]
     }
 )
