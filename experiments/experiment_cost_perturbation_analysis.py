4cd471a992cfd3199eabf4857b3f106ce632630e
Paras Jain <parasj96@gmail.com>
2019-12-12 22:33:16 -0800

    Throw an exception in SceduleBuilder rather than an assert, fix bad build from perturbation experiments (#95)
    * Throw an exception in SceduleBuilder rather than an assert
    
    * Clean up perturbation tests
    
    * Update experiment_cost_perturbation_analysis.py

--- experiments/experiment_cost_perturbation_analysis.py
+++ experiments/experiment_cost_perturbation_analysis.py
@@ -1,32 +1,31 @@
-import logging
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-from experiments.common.load_keras_model import get_keras_model
-from remat.core.solvers.strategy_checkpoint_all import solve_checkpoint_all
-from remat.tensorflow2.extraction import dfgraph_from_keras
 import seaborn as sns
-try:
-    import gurobipy as _
-except ImportError as e:
-    logging.exception(e)
-    logging.warning("Continuing with tests, gurobi not installed")
-    return
-from remat.core.solvers.strategy_optimal_ilp import solve_ilp_gurobi
 
-# get sample network and generate a graph on it
-model = get_keras_model("VGG16")
-g = dfgraph_from_keras(mod=model)
-assert g.size_fwd == 6
-budget = sum(g.cost_ram.values()) + g.cost_ram_parameters
+from checkmate.core.solvers.strategy_optimal_ilp import solve_ilp_gurobi
+from checkmate.tf2_keras.extraction import dfgraph_from_keras
+from experiments.common.definitions import checkmate_data_dir
+from experiments.common.load_keras_model import get_keras_model
 
-# solve for a schedule
-scheduler_result = solve_ilp_gurobi(g, budget)
-R = scheduler_result.schedule_aux_data.R
+if __name__ == '__main__':
+    # get sample network and generate a graph on it
+    model = get_keras_model("VGG16")
+    g = dfgraph_from_keras(mod=model)
+    budget = sum(g.cost_ram.values()) + g.cost_ram_parameters
 
-# compute costs for 1000 runs
-r = R.sum(axis=0)
-C @ r
-results = [np.random.normal(C, 1e7) @ r for i in range(1000)]
-plt.figure()
-x = pd.Series(results, name="Cost in flops")
-sns.distplot(x)
-plt.savefig('distribution_of_perturbed_c.png')
+    # solve for a schedule
+    scheduler_result = solve_ilp_gurobi(g, budget)
+    R = scheduler_result.schedule_aux_data.R
+
+    # compute costs for 1000 runs
+    r = R.sum(axis=0)
+    C = [g.cost_cpu[key] for key in sorted(g.cost_cpu)]
+    results = [np.random.normal(C, 1e7) @ r for i in range(1000)]
+    x = pd.Series(results, name="Cost in flops")
+
+    # plot costs
+    plt.figure()
+    sns.distplot(x)
+    checkmate_data_dir().mkdir(parents=True, exist_ok=True)
+    plt.savefig(checkmate_data_dir() / 'distribution_of_perturbed_cpu_costs.pdf')
