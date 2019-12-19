65b9953d317a517d2e7fb82631021a0cd0b6d488
Ajay Jain <ajayj@berkeley.edu>
2019-12-18 19:33:24 -0800

    Finish approx comparison experiment

--- experiments/experiment_approxcomparison.py
+++ experiments/experiment_approxcomparison.py
@@ -1,28 +1,72 @@
+import argparse
 import logging
+import pickle
 
-from checkmate.core.graph_builder import gen_linear_graph
-from checkmate.core.solvers.strategy_approx_lp import solve_approx_lp_deterministic_05_threshold, solve_approx_lp_randomized
-from experiments.common.definitions import checkmate_data_dir
-from experiments.common.graph_plotting import plot_schedule
-from checkmate.core.enum_strategy import SolveStrategy
-from checkmate.core.solvers.strategy_checkpoint_all import solve_checkpoint_all
-from checkmate.core.solvers.strategy_chen import solve_chen_sqrtn
-from checkmate.core.solvers.strategy_optimal_ilp import solve_ilp_gurobi
-from checkmate.core.utils.timer import Timer
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
-if __name__ == "__main__":
-    N = 16
-    B = 8
+from experiments.common.profile.platforms import PLATFORM_CHOICES, platform_memory, pretty_platform_name
+from experiments.common.profile.cost_model import CostModel
+from experiments.common.load_keras_model import MODEL_NAMES, get_keras_model, CHAIN_GRAPH_MODELS
+from experiments.common.graph_plotting import plot_schedule
+from experiments.common.definitions import checkmate_data_dir
+from checkmate.tf2_keras.extraction import dfgraph_from_keras
+from checkmate.core.utils.timer import Timer
+from checkmate.core.solvers.strategy_optimal_ilp import solve_ilp_gurobi
+from checkmate.core.solvers.strategy_chen import solve_chen_sqrtn
+from checkmate.core.solvers.strategy_checkpoint_all import solve_checkpoint_all
+from checkmate.core.solvers.strategy_approx_lp import solve_approx_lp_deterministic_05_threshold, solve_approx_lp_randomized
+from checkmate.core.graph_builder import gen_linear_graph
+from checkmate.core.enum_strategy import SolveStrategy
 
-    # model = get_keras_model("MobileNet")
-    # g = dfgraph_from_keras(mod=model)
-    g = gen_linear_graph(N)
-    scratch_dir = checkmate_data_dir() / "approxcomparison" / f"linear{N}" / f"budget{B}"
-    scratch_dir.mkdir(parents=True, exist_ok=True)
-    data = []
+
+LINEAR_MODELS = ["linear16", "VGG16", "VGG19", "MobileNet", "MobileNetV2"]
+
+
+def extract_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--platform", default="flops", choices=PLATFORM_CHOICES)
+    parser.add_argument("--model-name", default="VGG16", choices=list(sorted(MODEL_NAMES)) + ["linear16"])
+    parser.add_argument("-b", "--batch-size", type=int, default=256)
+    parser.add_argument("-nr", "--num-rounds", type=int, default=256)
+    parser.add_argument("--skip-ilp", action="store_true", help="If set, skip running the ILP during evaluation.")
+    return parser.parse_args()
+
+def b2gb(data):
+    if hasattr(data, "__iter__"):
+        return [d * 1e-9 for d in data]
+    return data * 1e-9
+
+if __name__ == "__main__":
+    args = extract_params()
+
+    if args.model_name == "linear16":
+        N = 16
+        B = 8
+        scratch_dir = checkmate_data_dir() / "approxcomparison" / args.model_name / f"budget{B}"
+        scratch_dir.mkdir(parents=True, exist_ok=True)
+
+        g = gen_linear_graph(N)
+    else:
+        B = platform_memory(args.platform)
+        scratch_dir = checkmate_data_dir() / "approxcomparison" / args.model_name / f"budget{B}"
+        scratch_dir.mkdir(parents=True, exist_ok=True)
+
+        # load costs, and plot optionally, if platform is not flops
+        print("Loading costs")
+        if args.platform == "flops":
+            cost_model = None
+        else:
+            cost_model = CostModel(args.model_name, args.platform, scratch_dir, quantization=5)
+            cost_model.fit()
+
+        # load model from Keras
+        print("Loading model {}".format(args.model_name))
+        model = get_keras_model(args.model_name)
+        g = dfgraph_from_keras(model, batch_size=args.batch_size, cost_model=cost_model,
+                               loss_cpu_cost=0, loss_ram_cost=(4 * args.batch_size))
 
     common_kwargs = dict(
         g=g,
@@ -32,7 +76,13 @@ if __name__ == "__main__":
         approx=False,
     )
 
+    print("Common args:", common_kwargs)
+
+    data = []
+
+    # Checkpoint all
     scheduler_result_all = solve_checkpoint_all(g)
+    plot_schedule(scheduler_result_all, False, save_file=scratch_dir / "ALL.png")
     data.append(
         {
             "Strategy": str(scheduler_result_all.solve_strategy.value),
@@ -42,37 +92,49 @@ if __name__ == "__main__":
         }
     )
 
-    scheduler_result_sqrtn = solve_chen_sqrtn(g, True)
-    data.append(
-        {
-            "Strategy": str(scheduler_result_sqrtn.solve_strategy.value),
-            "Name": "CHEN_SQRTN",
-            "CPU": scheduler_result_sqrtn.schedule_aux_data.cpu,
-            "Activation RAM": scheduler_result_sqrtn.schedule_aux_data.activation_ram,
-        }
-    )
-
-    with Timer("ilp") as timer_ilp:
-        scheduler_result_ilp = solve_ilp_gurobi(
-            g, B,
-            seed_s=scheduler_result_sqrtn.schedule_aux_data.S,
-            write_log_file=scratch_dir / "ilp.log",
-            print_to_console=False,
-        )
+    if args.model_name in LINEAR_MODELS:
+        # Sqrt(n)
+        scheduler_result_sqrtn = solve_chen_sqrtn(g, True)
+        plot_schedule(scheduler_result_sqrtn, False, save_file=scratch_dir / "SQRTN.png")
         data.append(
             {
-                "Strategy": str(scheduler_result_ilp.solve_strategy.value),
-                "Name": "CHECKMATE_ILP",
-                "CPU": scheduler_result_ilp.schedule_aux_data.cpu,
-                "Activation RAM": scheduler_result_ilp.schedule_aux_data.activation_ram,
+                "Strategy": str(scheduler_result_sqrtn.solve_strategy.value),
+                "Name": "CHEN_SQRTN",
+                "CPU": scheduler_result_sqrtn.schedule_aux_data.cpu,
+                "Activation RAM": scheduler_result_sqrtn.schedule_aux_data.activation_ram,
             }
         )
 
+    if not args.skip_ilp:
+        # ILP
+        scheduler_result_ilp = solve_ilp_gurobi(
+            g=g,
+            budget=B,
+            seed_s=scheduler_result_sqrtn.schedule_aux_data.S if args.model_name in LINEAR_MODELS else None,
+            write_log_file=scratch_dir / "ilp.log",
+            print_to_console=False,
+            eps_noise=0,
+            approx=False
+        )
+        if scheduler_result_ilp.schedule_aux_data is not None:
+            plot_schedule(scheduler_result_ilp, False, save_file=scratch_dir / "CHECKM8_ILP.png")
+            data.append(
+                {
+                    "Strategy": str(scheduler_result_ilp.solve_strategy.value),
+                    "Name": "CHECKMATE_ILP",
+                    "CPU": scheduler_result_ilp.schedule_aux_data.cpu,
+                    "Activation RAM": scheduler_result_ilp.schedule_aux_data.activation_ram,
+                }
+            )
+
+    # Deterministic rounding
     scheduler_lp_det = solve_approx_lp_deterministic_05_threshold(
         write_log_file=scratch_dir / "lp_det_05.log",
+        allow_return_infeasible_schedule=True,
         **common_kwargs
     )
     if scheduler_lp_det.schedule_aux_data is not None:
+        plot_schedule(scheduler_lp_det, False, save_file=scratch_dir / "CHECKM8_DET_APPROX_05.png")
         data.append({
                 "Strategy": str(scheduler_lp_det.solve_strategy.value),
                 "Name": "CHECKM8_DET_APPROX_05",
@@ -80,13 +142,15 @@ if __name__ == "__main__":
                 "Activation RAM": scheduler_lp_det.schedule_aux_data.activation_ram,
         })
 
+    # Randomized rounding
     scheduler_lp_rand, rounding_stats = solve_approx_lp_randomized(
         write_log_file=scratch_dir / "lp_rand.log",
-        num_rounds=500,
+        num_rounds=args.num_rounds,
         return_rounds=True,
         **common_kwargs
     )
     if scheduler_lp_rand.schedule_aux_data is not None:
+        plot_schedule(scheduler_lp_rand, False, save_file=scratch_dir / "CHECKM8_RAND_APPROX.png")
         data.append({
                 "Strategy": str(scheduler_lp_rand.solve_strategy.value),
                 "Name": "CHECKM8_RAND_APPROX",
@@ -99,36 +163,49 @@ if __name__ == "__main__":
     sns.set()
     sns.set_style("white")
 
-    plt.figure()
-    plt.xlabel("Activation memory usage")
-    plt.ylabel("Cost")
-
-    _, marker, markersize = SolveStrategy.get_plot_params(scheduler_lp_rand.solve_strategy)
-    plt.scatter(rounding_stats["activation_ram"], rounding_stats["cpu"],
-                s=markersize ** 2, color="lightcoral", marker=marker, label="Randomized rounding")
-
-    color, marker, markersize = SolveStrategy.get_plot_params(scheduler_lp_det.solve_strategy)
-    plt.scatter([scheduler_lp_det.schedule_aux_data.activation_ram], [scheduler_lp_det.schedule_aux_data.cpu],
-                s=markersize ** 2, color="royalblue", marker=marker, label="Deterministic rounding")
-
-    color, marker, markersize = SolveStrategy.get_plot_params(scheduler_result_sqrtn.solve_strategy)
-    plt.scatter([scheduler_result_sqrtn.schedule_aux_data.activation_ram], [scheduler_result_sqrtn.schedule_aux_data.cpu],
-                s=markersize ** 2, color=color, marker=marker, label="Chen $\sqrt{n}$")
+    plt.figure(figsize=(4, 4))
+    plt.xlabel("Activation memory usage (GB)")
+    plt.ylabel("GPU time (ms)")
 
     color, marker, markersize = SolveStrategy.get_plot_params(scheduler_result_all.solve_strategy)
-    plt.scatter([scheduler_result_all.schedule_aux_data.activation_ram], [scheduler_result_all.schedule_aux_data.cpu],
-                s=markersize ** 2, color=color, marker=marker, label="Checkpoint all (ideal)")
+    plt.axhline(y=scheduler_result_all.schedule_aux_data.cpu / 1000, color=color, linestyle='--', label="Checkpoint all (ideal)")
 
-    color, marker, markersize = SolveStrategy.get_plot_params(scheduler_result_ilp.solve_strategy)
-    plt.scatter([scheduler_result_ilp.schedule_aux_data.activation_ram], [scheduler_result_ilp.schedule_aux_data.cpu],
-                s=markersize ** 2, color=color, marker=marker, label="ILP")
+    if args.model_name in LINEAR_MODELS:
+        color, marker, markersize = SolveStrategy.get_plot_params(scheduler_result_sqrtn.solve_strategy)
+        plt.scatter([b2gb(scheduler_result_sqrtn.schedule_aux_data.activation_ram)], [scheduler_result_sqrtn.schedule_aux_data.cpu / 1000],
+                    s=markersize ** 2, color=color, marker=marker, label="Chen $\sqrt{n}$")
+
+    _, marker, markersize = SolveStrategy.get_plot_params(scheduler_lp_rand.solve_strategy)
+    plt.scatter(b2gb(rounding_stats["activation_ram"]), np.array(rounding_stats["cpu"]) / 1000,
+                s=markersize ** 2, color="lightcoral", marker=marker, label="Randomized rounding")
+    plt.axhline(y=np.mean(rounding_stats["cpu"]) / 1000, color="lightcoral", linestyle=':')
+
+    color, marker, markersize = SolveStrategy.get_plot_params(scheduler_lp_det.solve_strategy)
+    plt.scatter([b2gb(scheduler_lp_det.schedule_aux_data.activation_ram)], [scheduler_lp_det.schedule_aux_data.cpu / 1000],
+                s=markersize ** 2, color="royalblue", marker=marker, label="Deterministic rounding")
+
+    if not args.skip_ilp:
+        color, marker, markersize = SolveStrategy.get_plot_params(scheduler_result_ilp.solve_strategy)
+        plt.scatter([b2gb(scheduler_result_ilp.schedule_aux_data.activation_ram)], [scheduler_result_ilp.schedule_aux_data.cpu / 1000],
+                    s=markersize ** 2, color=color, marker=marker, label="ILP")
 
     plt.legend()
-    plt.tight_layout()
-    plt.savefig(scratch_dir / "scatter.png")
+    plt.savefig(scratch_dir / "scatter.pdf",
+        bbox_inches="tight",
+        format="pdf")
+    plt.savefig(scratch_dir / "scatter.png",
+        bbox_inches="tight",
+        dpi=300)
 
-
+    # Save results
     df = pd.DataFrame(data)
     df.to_csv(scratch_dir / "data.csv")
     df.plot.barh(y="CPU", x="Name")
     plt.savefig(scratch_dir / "barplot.png")
+
+    # Save data
+    with open(scratch_dir / "data.pickle", "wb") as f:
+        pickle.dump({
+            "data": data,
+            "rounding_stats": rounding_stats
+        }, f, protocol=pickle.HIGHEST_PROTOCOL)
