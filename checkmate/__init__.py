3b95601b6e35e62cd157a1ac408e0ca55c46d014
Paras Jain <parasj96@gmail.com>
2020-01-14 21:01:22 -0800

    Attempt at fixing namespace issue (#114)

--- checkmate/__init__.py
+++ checkmate/__init__.py
@@ -1,2 +1,2 @@
-from checkmate.tf2.load_keras_model import get_keras_model
-from checkmate.tf2.wrapper import compile_tf2
+from .tf2.load_keras_model import get_keras_model
+from .tf2.wrapper import compile_tf2
